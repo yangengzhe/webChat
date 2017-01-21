2e5547505b3565b59db49d83ad89ae155bff5a09
yangengzhe <emailvip@vip.qq.com>
2017-01-21 13:20:08 +0800

    Mybatis生成工具

---
+++ sql/mybatis/src/com/ices/yangengzhe/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ices.yangengzhe.dao;
+
+import com.ices.yangengzhe.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
